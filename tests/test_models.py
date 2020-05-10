fcca6aeb8ab07b1d6c4af99620b60166b2bd6161
Jeff Albrecht <geospatialjeff@gmail.com>
2020-05-09 19:13:31 -0500

    test supportedCRS validation error

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,8 @@
+import json
 import os
 
 import pytest
+from pydantic import ValidationError
 
 from morecantile.models import TileMatrixSet
 from rasterio.crs import CRS
@@ -18,3 +20,12 @@ def test_tile_matrix_set(tileset):
     # This would fail if `supportedCRS` isn't valid EPSG code
     epsg = ts.crs
     isinstance(epsg, CRS)
+
+
+def test_tms_not_in_epsg():
+    with open(tilesets[0]) as f:
+        data = json.load(f)
+        # Replace supportedCRS with CRS84 which isn't defined in EPSG
+        data["supportedCRS"] = "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
+        with pytest.raises(ValidationError):
+            TileMatrixSet(**data)
