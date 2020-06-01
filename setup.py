585d31b3f617034611de9816ab0d2537047fe139
vincentsarago <vincent.sarago@gmail.com>
2020-06-01 09:44:16 -0400

    release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extra_reqs = {
 
 setup(
     name="morecantile",
-    version="1.1.1",
+    version="1.2.0",
     python_requires=">=3",
     description=u"""Construct and use map tile grids (a.k.a TileMatrixSet / TMS).""",
     long_description=long_description,
