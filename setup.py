4ed4b146a4f52cbf35b885641b13b4e97270a62a
vincentsarago <vincent.sarago@gmail.com>
2022-02-16 11:09:15 +0100

    Bump version: 3.0.3 → 3.0.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extra_reqs = {
 
 setup(
     name="morecantile",
-    version="3.0.3",
+    version="3.0.4",
     python_requires=">=3.7",
     description=u"""Construct and use map tile grids (a.k.a TileMatrixSet / TMS).""",
     long_description=long_description,
